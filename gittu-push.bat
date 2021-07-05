f337fb3971f5a48bc3a396b83d177ab738794046
NaZmuZ-SaKiB <sakibnazmuz01@gmail.com>
2021-07-05 11:52:57 +0600

    removed experience style

--- gittu-push.bat
+++ gittu-push.bat
@@ -1,10 +1,18 @@
 @echo off
-echo gittu paking
-git add .
-echo now starting commit
 set /P commit=Enter your Commit Description :- 
+echo ------------------
+echo -- gittu paking --
+echo ------------------
+git add .
+echo ---------------------------
+echo --- now starting commit ---
+echo ---------------------------
 git commit -m "%commit%"
-echo Gittu Mia  pusshing
+echo ---------------------------
+echo --- Gittu Mia  pusshing ---
+echo ---------------------------
 git push
-echo Gittu pakano shesh
+echo --------------------------
+echo ------- গিট্টু পাকানো শেষ --------------
+echo --------------------------
 pause
